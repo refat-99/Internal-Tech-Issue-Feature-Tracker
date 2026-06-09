66b520ad7d50e083d7ae06c1c0f0e103d8c1ffc7
Md Refadul Islam <refadulislam99@gmail.com>
2026-06-09 23:40:59 +0600

    env-neondb-setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,39 @@
-import express, {type Request, type Response, type Application } from 'express';
-const app : Application = express();
-const port = 5000;
+import express, {
+  type Request,
+  type Response,
+  type Application,
+} from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-app.get('/', (req: Request, res: Response) => {
-  //res.send('Issue Traker Server is running!');
+dotenv.config();
 
+import config from "./config/db";
+
+const app: Application = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+// Database Connection
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+// Port
+const port = config.port || 5000;
+
+// Route
+app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({
-    message: 'Issue Tracker Server is running!',
-    timestamp: new Date().toISOString()
+    message: "Issue Tracker Server is running!",
+    timestamp: new Date().toISOString(),
   });
 });
 
+// Server Start
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
